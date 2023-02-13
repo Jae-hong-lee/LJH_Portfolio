b22f5ac3ad7b136e53ddeaa19b739886b54d715c
Jae-hong-Lee <keash8091@gmail.com>
2023-02-13 22:06:44 +0900

    fix: github action Error, Error occurred prerendering page('')

--- src/component/project/project.presenter.tsx
+++ src/component/project/project.presenter.tsx
@@ -2,14 +2,14 @@ import { Card, Col, Divider, Row } from "antd";
 import * as PS from "./project.styles";
 
 export default function ProjectUI(props: any) {
-  console.log("UI 페이지", props.projects.results);
+  console.log("UI 페이지", props.projects?.results);
 
   return (
     <PS.Wrapper>
       <Divider orientation="center">
         <PS.Projectlength>
           총 프로젝트 :{" "}
-          <PS.ProjectNum>{props.projects.results.length}</PS.ProjectNum>
+          <PS.ProjectNum>{props.projects?.results.length}</PS.ProjectNum>
         </PS.Projectlength>
       </Divider>
 
